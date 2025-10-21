d8a2bc20cd0aadf92adccaa5e3417fe233220c27
cblopez001 <cherakye.lopez@gmail.com>
2025-10-20 22:37:51 -0400

    added content to my two components: NavBar.js Hero.js

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Hero = () =>(
+    <section id="hero" className="hero-section">
+        <h1>Hi! I'm Cherry Lopez</h1>
+        <h2>Welcome to my portfolio!</h2>
+    </section>
+);
+
+export default Hero;
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => (
+    <nav>
+        <ul>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Resume</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+);
+
+export default NavBar;
