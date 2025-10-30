987a4bc8bee3b8d365bf8e191667d85b76091216
Cherakye Lopez <163934607+cblopez001@users.noreply.github.com>
2025-10-30 09:58:22 -0400

    Update NavBar.jsx
    placed Experience button before the Projects button. I will need to make the same changes in the App.jsx file.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,8 @@ export default function Navbar({ setSection }) {
     <nav style={{ padding: '1rem', position: 'sticky', top: 0, backgroundColor: 'black', zIndex: 10 }}>
       <button onClick={() => setSection('hero')} style={{ marginRight: '1rem' }}>Home</button>
       <button onClick={() => setSection('about')} style={{ marginRight: '1rem' }}>About</button>
-      <button onClick={() => setSection('projects')} style={{ marginRight: '1rem' }}>Projects</button>
       <button onClick={() => setSection('experience')} style={{ marginRight: '1rem' }}>Experience</button>
+      <button onClick={() => setSection('projects')} style={{ marginRight: '1rem' }}>Projects</button>
       <button onClick={() => setSection('contact')}>Contact</button>
     </nav>
   );
